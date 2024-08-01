29746ecaeb399060b6acc2d6bb8977103150ab91
eximus <fabio4335@gmail.com>
2024-08-01 11:20:27 -0400

    don't check for previously uploaded checksum, there's no point

--- scripts/upload_to_vagrant_cloud.sh
+++ scripts/upload_to_vagrant_cloud.sh
@@ -19,30 +19,27 @@ curl -X POST \
   https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/versions \
   --data "{ \"version\": { \"version\": \"${BOX_VERSION}\", \"description\": \"$(./scripts/describe_packages.sh)\" } }"
 
-CHECKSUM_BOX_FILE=$(sha256sum "${VAGRANT_BOX_FILE}" | cut -d ' ' -f 1)
-UPLOADED_BOX_CHECKSUM=$(curl -s "https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/provider/libvirt" | jq -r '.checksum')
-echo $CHECKSUM_BOX_FILE
-echo $UPLOADED_BOX_CHECKSUM
-if [[ ${UPLOADED_BOX_CHECKSUM} != "${CHECKSUM_BOX_FILE}" ]]; then
-  # create box provider
-  curl -X POST \
-    -H "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-    https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/providers \
-    --json "{ \"provider\": { \"name\": \"libvirt\", \"checksum\": \"${CHECKSUM_BOX_FILE}\", \"checksum_type\": \"sha256\", \"architecture\": \"amd64\" } }"
+BOX_CHECKSUM=$(sha256sum "${VAGRANT_BOX_FILE}" | cut -d ' ' -f 1)
+
+# create box provider
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
+  https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/providers \
+  --data "{ \"provider\": { \"name\": \"libvirt\", \"checksum\": \"${BOX_CHECKSUM}\", \"checksum_type\": \"sha256\", \"architecture\": \"amd64\" } }"
 
 
-  # get upload path
-  upload_path=$(curl -s \
-    -H "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-    https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/provider/libvirt/amd64/upload \
-    | jq -r .upload_path)
+# get upload path
+upload_path=$(curl -s \
+  -H "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
+  https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/provider/libvirt/amd64/upload \
+  | jq -r .upload_path)
 
-  # upload box
-  curl -X PUT --upload-file ${VAGRANT_BOX_FILE} "${upload_path}"
+# upload box
+curl -X PUT --upload-file ${VAGRANT_BOX_FILE} "${upload_path}"
 
-  # release version
-  curl -X PUT \
-    -H "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
-    https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/release
-fi
+# release version
+curl -X PUT \
+  -H "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" \
+  https://app.vagrantup.com/api/v2/box/${VAGRANT_CLOUD_USER}/${NAME}/version/${BOX_VERSION}/release
 
